cfd9aaf1990531541dfae069249889545f6e83d3
OperationKen <kennethcpohcs@gmail.com>
2021-08-05 13:51:10 -0500

    Initial commit

---
+++ flight.sql
@@ -0,0 +1,51 @@
+create table flight (
+	id INT NOT NULL AUTO_INCREMENT,
+	from_airport VARCHAR(50),
+	to_airport VARCHAR(50),
+	departure VARCHAR(50),
+	arrival VARCHAR(50),
+	flight_number VARCHAR(50),
+    PRIMARY KEY (id)
+);
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (1, 'USD', 'CNY', '2020-11-15', '2021-03-08', '0363-0098');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (2, 'RUB', 'IDR', '2021-04-23', '2020-12-27', '63776-653');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (3, 'EUR', 'CNY', '2021-06-03', '2021-05-22', '43553-3256');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (4, 'NGN', 'CNY', '2020-12-16', '2021-05-07', '49349-931');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (5, 'SEK', 'EUR', '2020-09-04', '2021-03-04', '0268-6198');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (6, 'PEN', 'PHP', '2021-05-16', '2020-09-12', '0069-5810');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (7, 'UAH', 'RUB', '2021-01-12', '2021-03-18', '51785-866');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (8, 'RUB', 'ZAR', '2021-06-03', '2020-11-06', '76296-005');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (9, 'IDR', 'IDR', '2021-04-23', '2021-05-14', '55154-4945');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (10, 'BIF', 'CNY', '2021-07-22', '2020-08-16', '41250-177');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (11, 'MXN', 'CNY', '2021-01-03', '2021-06-14', '65044-3381');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (12, 'EUR', 'PLN', '2021-04-23', '2021-02-04', '0268-0615');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (13, 'BGN', 'CNY', '2020-10-01', '2020-10-29', '68437-011');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (14, 'CNY', 'RUB', '2020-09-19', '2020-12-26', '53614-001');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (15, 'IDR', 'PHP', '2020-09-17', '2020-09-08', '0363-0519');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (16, 'CNY', 'TZS', '2021-01-20', '2021-05-22', '13537-548');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (17, 'PHP', 'IDR', '2021-01-27', '2020-09-19', '59779-708');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (18, 'UAH', 'PHP', '2020-09-21', '2021-04-06', '51885-3310');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (19, 'NOK', 'LRD', '2020-12-11', '2020-11-22', '36987-2519');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (20, 'EUR', 'EUR', '2020-12-11', '2021-03-30', '33342-036');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (21, 'AMD', 'IDR', '2020-09-25', '2021-02-12', '16590-781');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (22, 'KES', 'CNY', '2021-01-28', '2021-06-06', '42291-598');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (23, 'XCD', 'IDR', '2021-07-29', '2020-12-17', '67345-0781');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (24, 'EUR', 'CAD', '2020-09-03', '2020-12-10', '0781-3182');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (25, 'IDR', 'PHP', '2020-10-30', '2020-10-05', '36987-1651');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (26, 'IDR', 'XOF', '2021-06-13', '2021-01-26', '55143-108');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (27, 'PHP', 'EUR', '2020-10-17', '2021-02-13', '54868-5616');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (28, 'EUR', 'IDR', '2020-08-16', '2021-03-12', '75862-020');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (29, 'SEK', 'MXN', '2021-08-01', '2021-05-27', '49738-530');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (30, 'LKR', 'CNY', '2020-08-24', '2020-10-12', '10812-351');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (31, 'PHP', 'BRL', '2021-02-13', '2021-05-08', '68645-486');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (32, 'PLN', 'PLN', '2021-04-20', '2020-11-21', '62175-154');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (33, 'CNY', 'RUB', '2021-02-05', '2021-04-08', '11534-155');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (34, 'EUR', 'CNY', '2021-05-02', '2020-10-23', '55154-2351');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (35, 'CNY', 'CNY', '2021-01-24', '2020-08-19', '43406-0070');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (36, 'RUB', 'USD', '2021-02-19', '2021-04-16', '0904-6350');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (37, 'KRW', 'RUB', '2020-09-26', '2020-09-24', '53152-1008');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (38, 'KZT', 'USD', '2020-11-24', '2021-05-15', '55111-525');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (39, 'EUR', 'CNY', '2020-09-20', '2020-09-26', '51672-2097');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (40, 'BRL', 'RUB', '2021-01-25', '2021-04-05', '63699-001');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (41, 'CNY', 'EUR', '2021-05-20', '2020-08-12', '55154-4764');
+insert into flight (id, from_airport, to_airport, departure, arrival, flight_number) values (42, 'CNY', 'CZK', '2021-01-11', '2021-05-09', '49349-546');
